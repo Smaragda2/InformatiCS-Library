851d93dc3a90758a14081157fa4f3bb93e5f922a
Smaragda Prasianaki <smaragda2@hotmail.com>
2018-12-21 13:34:47 +0200

    Implement Lucene Search Engine and Search in Category

--- testadopse/testadopse/UserControls/HomeUC.cs
+++ testadopse/testadopse/UserControls/HomeUC.cs
@@ -7,29 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using testadopse.InformatiCS_LibraryDataSet1TableAdapters;
 
 namespace testadopse.UserControls
 {
     public partial class HomeUC : UserControl
     {
+        //static string connectionString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source =| DataDirectory |\\InformatiCS_Library.mdb";
+        Searcher searcher = new Searcher();
         public HomeUC()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+           
+            if(textBox1.Text != "")
+            {
+                if (radioButton1.Checked || radioButton2.Checked)
+                {
+                    label3.Text = "";
+                    string[] results = searcher.Search(textBox1.Text);
+                    for(int i = 0; i < results.Length; i++)
+                    {
+                        label3.Text += results[i]+"\n";
+                    }
+                    //search.SearchInTitle(textBox1, label3);
+                }
+                else if (radioButton3.Checked)
+                {
+                    label3.Text = "";
+                    string[] results = searcher.SearchByCategory(textBox1.Text);
+                    for (int i = 0; i < results.Length; i++)
+                    {
+                        label3.Text += results[i] + "\n";
+                    }
+                }
+            }
+            else
+            {
+                //label3.Visible = false;
+            }
         }
 
-        private void label1_Click_1(object sender, EventArgs e)
+        private void listBox1_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
+           //textBox1.Text = listBox1.SelectedItem.ToString();
         }
     }
 }
