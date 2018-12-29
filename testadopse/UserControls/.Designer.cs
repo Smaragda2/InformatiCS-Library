2ffc98d8995ca0f07bd3fc608a8835049d193776
kionnid <44463980+kionnid@users.noreply.github.com>
2018-12-29 16:35:48 +0200

    Add files via upload

---
+++ testadopse/UserControls/.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace testadopse.UserControls
+{
+    partial class CategoriesUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Category 1");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Category 2");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Category 3");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Category 4");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Category 5");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Category 6");
+            this.CategoriesP = new System.Windows.Forms.Panel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.viewUC1 = new testadopse.UserControls.ViewUC();
+            this.CategoriesP.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CategoriesP
+            // 
+            this.CategoriesP.Controls.Add(this.treeView1);
+            this.CategoriesP.Dock = System.Windows.Forms.DockStyle.Left;
+            this.CategoriesP.ForeColor = System.Drawing.Color.Black;
+            this.CategoriesP.Location = new System.Drawing.Point(0, 0);
+            this.CategoriesP.Name = "CategoriesP";
+            this.CategoriesP.Size = new System.Drawing.Size(172, 661);
+            this.CategoriesP.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Category 1";
+            treeNode2.Name = "Node1";
+            treeNode2.Text = "Category 2";
+            treeNode3.Name = "Node2";
+            treeNode3.Text = "Category 3";
+            treeNode4.Name = "Node3";
+            treeNode4.Text = "Category 4";
+            treeNode5.Name = "Node4";
+            treeNode5.Text = "Category 5";
+            treeNode6.Name = "Node5";
+            treeNode6.Text = "Category 6";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            this.treeView1.Size = new System.Drawing.Size(172, 661);
+            this.treeView1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.viewUC1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(172, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(944, 661);
+            this.panel2.TabIndex = 1;
+            // 
+            // viewUC1
+            // 
+            this.viewUC1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.viewUC1.BackColor = System.Drawing.Color.White;
+            this.viewUC1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.viewUC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.viewUC1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.viewUC1.Location = new System.Drawing.Point(0, 0);
+            this.viewUC1.Name = "viewUC1";
+            this.viewUC1.Size = new System.Drawing.Size(944, 661);
+            this.viewUC1.TabIndex = 0;
+            this.viewUC1.Load += new System.EventHandler(this.viewUC1_Load);
+            // 
+            // CategoriesUC
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.CategoriesP);
+            this.Name = "CategoriesUC";
+            this.Size = new System.Drawing.Size(1116, 661);
+            this.CategoriesP.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel CategoriesP;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TreeView treeView1;
+        private ViewUC viewUC1;
+    }
+}
